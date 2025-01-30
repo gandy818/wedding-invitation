e1ef6034b76a6b7b2918c6add742bd526de8e0aa
gandy818 <gandy818@gmail.com>
2025-01-30 16:53:21 +0900

    feat: 갤러리 추가

---
+++ src/app/Gallery/images.ts
@@ -0,0 +1,41 @@
+const images = [
+  /* 이미지 경로를 리스트로 저장 */
+  {
+    alt: 'image01',
+    source: '/images/gallery/01.jpeg',
+  },
+  {
+    alt: 'image02',
+    source: '/images/gallery/02.jpeg',
+  },
+  {
+    alt: 'image03',
+    source: '/images/gallery/03.jpeg',
+  },
+  {
+    alt: 'image04',
+    source: '/images/gallery/04.jpeg',
+  },
+  {
+    alt: 'image05',
+    source: '/images/gallery/05.jpeg',
+  },
+  {
+    alt: 'image06',
+    source: '/images/gallery/06.jpeg',
+  },
+  {
+    alt: 'image07',
+    source: '/images/gallery/07.jpeg',
+  },
+  {
+    alt: 'image08',
+    source: '/images/gallery/08.jpeg',
+  },
+  {
+    alt: 'image09',
+    source: '/images/gallery/09.jpeg',
+  },
+];
+
+export default images;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { Gallery, Item } from 'react-photoswipe-gallery';
+import 'photoswipe/dist/photoswipe.css';
+import images from './\bGallery/images';
+
 export default function Home() {
   const daysInDecember = 31;
   const startDay = 1; // 12월 1일이 금요일이므로 요일 인덱스 (0: 일요일, 1: 월요일, ... 6: 토요일)
@@ -76,8 +82,8 @@ export default function Home() {
                     className={`relative p-3 ${idx === 0 ? 'text-red-500' : 'text-gray-600'}`}
                   >
                     {day === weddingDay && (
-                      <div className="left-0 absolute p-3 rounded-full m- bg-red-200 top-0 w-full h-full text-white">
-                        {day || ''}
+                      <div className="bg-red-200 rounded-full w-full aspect-square absolute left-0 top-0.5">
+                        <span className="absolute text-white top-2.5 left-3">{day || ''}</span>
                       </div>
                     )}
 
@@ -88,6 +94,41 @@ export default function Home() {
             ))}
           </tbody>
         </table>
+      </div>
+
+      {/* 갤러리 */}
+      <div className="py-10">
+        <p className="text-4xl">GALLERY</p>
+        <p className="text-[#111] opacity-25 text-sm pt-4 pb-12">
+          사진을 클릭하시면 전체 화면 보기가 가능합니다
+        </p>
+
+        <Gallery>
+          <div className="mx-auto grid grid-cols-3 gap-1 px-10">
+            {images.map((image, index) => {
+              return (
+                <Item
+                  key={index}
+                  cropped
+                  original={image.source}
+                  thumbnail={image.source}
+                  width="1280"
+                  height="1920"
+                >
+                  {({ ref, open }) => (
+                    <img
+                      className="cursor-pointer w-25 h-38 max-w-none mx-auto object-cover"
+                      alt={image.alt}
+                      src={image.source}
+                      ref={ref}
+                      onClick={open}
+                    />
+                  )}
+                </Item>
+              );
+            })}
+          </div>
+        </Gallery>
       </div>
 
       {/* 오시는 길 */}
