d408a8f5cc903c6ee4069678698b4410da454b7a
gandy818 <gandy818@gmail.com>
2025-04-29 13:09:23 +0900

    feat: 컨피그 파일 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/wedding-invitation',
-  assetPrefix: '/wedding-invitation',
+  basePath: isProd ? '/wedding-invitation' : '',
+  assetPrefix: isProd ? '/wedding-invitation' : '',
 };
 
 export default nextConfig;
